4b52a3a5b1dccce70957e3ff317e9c7281c7fb9d
r42y9fcmjrawl <r42y9fcmjrawl@users.noreply.github.com>
2017-05-01 10:45:55 -0400

    uploaded start and stop

---
+++ stop.ps1
@@ -0,0 +1,26 @@
+﻿$serviceName = ""
+$vmName = ""
+
+# close PuTTY
+$putty = Get-Process -Name "putty" -ErrorAction SilentlyContinue
+if ($putty) {
+    Stop-Process $putty
+}
+
+# select azure subscription
+Get-AzureSubscription | Select SubscriptionID | Select-AzureSubscription
+
+# check vm status
+$state = (Get-AzureVM -ServiceName $serviceName -Name $vmName).PowerState
+
+# stop if not off
+if ($state -ne "Stopped") {
+    Get-AzureVM -ServiceName $serviceName -Name $vmName | Stop-AzureVM -StayProvisioned
+}
+
+# wait until shutdown
+do {
+    $state = (Get-AzureVM -ServiceName $serviceName -Name $vmName).PowerState
+    Start-Sleep -s 1
+} 
+until ($state -eq "Stopped")
